b1e034fb63850c3c44a1f1ad74f7d42dd9a7a751
Elya <mirademina22@gmail.com>
2023-02-28 11:45:56 +0300

    otvet dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write ("Введите число от 1 до 7");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("Будни");
+}
+
+else if (a == 2)
+
+{
+    Console.WriteLine("Будни");
+}
+
+else if (a == 3)
+
+{
+    Console.WriteLine("Будни");
+}
+
+else if (a == 4)
+
+{
+    Console.WriteLine("Будни");
+}
+
+else if (a == 5)
+
+{
+    Console.WriteLine("Будни");
+}
+
+else if (a == 6)
+
+{
+    Console.WriteLine("Выходной");
+}
+
+else
+
+{
+    Console.WriteLine("Выходной");
+}
+
+
